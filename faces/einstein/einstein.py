2745625bdc663e09a02456ea1263b98a3eff70ca
X-Xer0 <148498547+X-Xer0@users.noreply.github.com>
2024-10-03 12:12:31 +0000

    Thu, Oct 3, 2024, 5:42 PM +05:30

--- faces/einstein/einstein.py
+++ faces/einstein/einstein.py
@@ -1,8 +1,8 @@
 def main():
-    m = int(input())
+    m = int(input('m: '))
     c = 300000000
     energy = m * (c ** 2)
-    print(energy)
+    print('E', energy)
 
 if __name__ == "__main__":
     main()
