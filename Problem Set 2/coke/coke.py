b9faa0c59ad2b6b7a0b84952c56048fe2f8d32c4
X-Xer0 <148498547+X-Xer0@users.noreply.github.com>
2024-10-04 12:09:07 +0000

    Fri, Oct 4, 2024, 5:39 PM +05:30

--- Problem Set 2/coke/coke.py
+++ Problem Set 2/coke/coke.py
@@ -3,13 +3,13 @@ def main():
     amount_due = cost
 
     while amount_due > 0:
-        print(f"Amount Due: {amount_due} cents")
+        print(f"Amount Due: {amount_due}")
         coin = int(input("Insert coin: "))
         if coin in [25, 10, 5]:
             amount_due -= coin
 
     change = abs(amount_due)
-    print(f"Change Owed: {change} cents")
+    print(f"Change Owed: {change}")
 
 if __name__ == "__main__":
     main()
