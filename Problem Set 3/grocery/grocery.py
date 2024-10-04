41d861c18a85ef8eeec95200123679f2e4930f31
X-Xer0 <148498547+X-Xer0@users.noreply.github.com>
2024-10-04 12:34:28 +0000

    Fri, Oct 4, 2024, 6:04 PM +05:30

---
+++ Problem Set 3/grocery/grocery.py
@@ -0,0 +1,15 @@
+def main():
+    grocery_list = {}
+
+    while True:
+        try:
+            item = input("Item: ").strip().lower()
+            if item:
+                grocery_list[item] = grocery_list.get(item, 0) + 1
+        except EOFError:
+            break
+
+    for item in sorted(grocery_list):
+        print(f"{grocery_list[item]} {item.upper()}")
+
+main()
